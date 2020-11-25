fb76a9875eb70ffb97c1010cd4927d633e80ec39
lawsgame <jfweets1@gmail.com>
2020-11-25 04:10:40 +0100

    additions

--- src/com/test/main/Main.java
+++ src/com/test/main/Main.java
@@ -3,6 +3,7 @@ package com.test.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello 2");
     }
 
 }
